18c1247007b3caa1cb93d0e1b18a73ebce6b407b
Ondrej Lichtner <ilichtner@fit.vutbr.cz>
2024-06-22 10:29:00 +0200

    refactor to enable loading config from kafka
    Signed-off-by: Ondrej Lichtner <ilichtner@fit.vutbr.cz>

--- feta_prefilter/main.py
+++ feta_prefilter/main.py
@@ -1,7 +1,14 @@
+import os
 import sys
 import argparse
 import logging
+from pathlib import Path
+from pprint import pprint
+import json
 
+from kafka import KafkaConsumer, KafkaProducer
+
+from feta_prefilter.utils import make_ssl_context
 from feta_prefilter.Sources import source_classes
 from feta_prefilter.Filters import filter_classes
 from feta_prefilter.Outputs import output_classes
@@ -10,59 +17,17 @@ from feta_prefilter.Filters.BaseFilter import FilterAction
 
 logger = logging.getLogger(__name__)
 
+
 def main():
-    config = read_config()
+    config = init_config()
     logging.basicConfig(level=logging.INFO)
 
-    sources = []
-    for source in config["sources"]:
-        source_cls_name = source["type"]
-        if source_cls_name not in source_classes:
-            logger.error(f"Unknown source class type {source_cls_name}")
-            continue
-
-        try:
-            source_cls = source_classes[source_cls_name]
-            source_obj = source_cls(*source["args"], **source["kwargs"])
-        except:
-            logger.exception(f"Failed source initialization")
-            continue
-
-        sources.append(source_obj)
-
-    filters = []
-    for f in config["filters"]:
-        filter_cls_name = f["type"]
-        if filter_cls_name not in filter_classes:
-            logger.error(f"Unknown filter class type {filter_cls_name}")
-            continue
-
-        try:
-            filter_cls = filter_classes[filter_cls_name]
-            filter_obj = filter_cls(*f["args"], **f["kwargs"])
-        except:
-            logger.exception(f"Failed filter initialization")
-            continue
-
-        filters.append(filter_obj)
-
-    outputs = []
-    for output in config["outputs"]:
-        output_cls_name = output["type"]
-        if output_cls_name not in output_classes:
-            logger.error(f"Unknown output class type {output_cls_name}")
-            continue
-
-        try:
-            output_cls = output_classes[output_cls_name]
-            output_obj = output_cls(*output["args"], **output["kwargs"])
-        except:
-            logger.exception(f"Failed output initialization")
-            continue
-
-        outputs.append(output_obj)
+    sources, filters, outputs = create_app(config)
 
     while True:
+        if update_config(config):
+            sources, filters, outputs = create_app(config)
+
         domains = set()
         for s in sources:
             domains.update(s.collect())
@@ -83,19 +48,17 @@ def main():
         # ]
         filtered_domains = []
         for i, domain in enumerate(domains):
-            domain_results = {f.__class__.__name__: filter_results[f][i] for f in filter_results.keys()}
+            domain_results = {
+                f.__class__.__name__: filter_results[f][i]
+                for f in filter_results.keys()
+            }
             max_domain_result = max(domain_results.values())
             if max_domain_result == FilterAction.DROP:
                 continue
             elif max_domain_result == FilterAction.STORE:
-                filtered_domains.append(
-                    {
-                        "domain": domain,
-                        "f_results": domain_results
-                    }
-                )
+                filtered_domains.append({"domain": domain, "f_results": domain_results})
             else:
-                #if all are PASS-analyze, then we don't need to store the json
+                # if all are PASS-analyze, then we don't need to store the json
                 filtered_domains.append(
                     {
                         "domain": domain,
@@ -107,43 +70,146 @@ def main():
             o.output(filtered_domains)
 
 
-def read_config() -> dict:
-    return {
-        "sources": [
-            {
-                "type": "FileSource",
-                "args": [],
-                "kwargs": {
-                    "filename": "source.list",
-                },
-            },
-        ],
-        "filters": [
-            {
-                "type": "FileBlockListFilter",
-                "args": [],
-                "kwargs": {
-                    "filter_result_action": FilterAction.STORE,
-                    "filename": "block.list",
-                },
-            },
-            {
-                "type": "FileBlockListFilter",
-                "args": [],
-                "kwargs": {
-                    "filter_result_action": FilterAction.DROP,
-                    "filename": "block2.list",
-                },
-            },
-        ],
-        "outputs": [
-            {
-                "type": "StdOutput",
-                "args": [],
-                "kwargs": {},
-            },
-        ],
+def init_config() -> dict:
+    config = {
+        "kafka_broker": os.environ.get("DOMAINRADAR_KAFKA_BROKER_URL", ""),
+        "kafka_secrets_dir": os.environ.get("DOMAINRADAR_KAFKA_SECRETS_DIR", ""),
+        "dynamic_config": {
+            "sources": [],
+            "filters": [],
+            "outputs": [],
+        },
     }
+
+    kafka_consumer = config["kafka_consumer"] = KafkaConsumer(
+        "configuration_change_requests",
+        client_id="loader-consumer",
+        bootstrap_servers=config["kafka_broker"],
+        security_protocol="SSL",
+        ssl_context=make_ssl_context(Path(config["kafka_secrets_dir"])),
+        auto_offset_reset="earliest",
+        consumer_timeout_ms=500,
+    )
+
+    config["kafka_producer"] = KafkaProducer(
+        client_id="loader-producer",
+        bootstrap_servers=config["kafka_broker"],
+        security_protocol="SSL",
+        ssl_context=make_ssl_context(Path(config["kafka_secrets_dir"])),
+    )
+
+    if not update_config(config):
+        # if we didn't get any config messages from kafka we send the empty default
+        notify_config_change(True, config)
+    return config
+
+
+def update_config(config: dict) -> bool:
+    changed = False
+    consumer = config["kafka_consumer"]
+    for msg in consumer:
+        logger.debug(msg)
+        if msg.key.decode() != "loader":
+            continue
+        change_request = json.loads(msg.value)
+        logger.info(f"Change request received: {change_request}")
+        if validate_dynamic_config(change_request):
+            config["dynamic_config"] = change_request
+            success = True
+            changed = True
+        else:
+            success = False
+
+        notify_config_change(success, config)
+
+    return changed
+
+
+def notify_config_change(success: bool, config: dict):
+    producer = config["kafka_producer"]
+    msg = {
+        "success": success,
+        "errors": None,
+        "message": None,
+        "currentConfig": config["dynamic_config"],
+    }
+
+    producer.send(
+        "configuration_states", key="loader".encode(), value=json.dumps(msg).encode()
+    )
+    return
+
+
+def create_app(config: dict):
+    sources = []
+    for source in config["dynamic_config"]["sources"]:
+        source_cls_name = source["type"]
+        if source_cls_name not in source_classes:
+            logger.error(f"Unknown source class type {source_cls_name}")
+            continue
+
+        try:
+            source_cls = source_classes[source_cls_name]
+            source_obj = source_cls(*source["args"], **source["kwargs"])
+        except:
+            logger.exception(f"Failed source initialization")
+            continue
+
+        sources.append(source_obj)
+
+    filters = []
+    for f in config["dynamic_config"]["filters"]:
+        filter_cls_name = f["type"]
+        if filter_cls_name not in filter_classes:
+            logger.error(f"Unknown filter class type {filter_cls_name}")
+            continue
+
+        try:
+            filter_cls = filter_classes[filter_cls_name]
+            filter_obj = filter_cls(*f["args"], **f["kwargs"])
+        except:
+            logger.exception(f"Failed filter initialization")
+            continue
+
+        filters.append(filter_obj)
+
+    outputs = []
+    for output in config["dynamic_config"]["outputs"]:
+        output_cls_name = output["type"]
+        if output_cls_name not in output_classes:
+            logger.error(f"Unknown output class type {output_cls_name}")
+            continue
+
+        try:
+            output_cls = output_classes[output_cls_name]
+            output_obj = output_cls(*output["args"], **output["kwargs"])
+        except:
+            logger.exception(f"Failed output initialization")
+            continue
+
+        outputs.append(output_obj)
+    return sources, filters, outputs
+
+
+def validate_dynamic_config(change_request):
+    try:
+        for src in change_request["sources"]:
+            validate_config_block(src)
+        for f in change_request["filters"]:
+            validate_config_block(f)
+        for output in change_request["outputs"]:
+            validate_config_block(output)
+    except:
+        logger.exception(f"Failed validation of config change request {change_request}")
+        return False
+
+    return True
+
+
+def validate_config_block(block: dict):
+    block["type"]
+    block["args"]
+    block["kwargs"]
 
 
 if __name__ == "__main__":
---
+++ feta_prefilter/utils.py
@@ -0,0 +1,42 @@
+import logging
+import ssl
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+def password_loader(path: Path):
+    def loader():
+        with open(path) as f:
+            return f.read().strip()
+    return loader
+
+def make_ssl_context(path: Path) -> ssl.SSLContext | None:
+    """Creates an SSL context with the specified configuration."""
+    logger.info("Loading SSL configuration")
+
+    ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    ssl_context.options |= ssl.OP_NO_SSLv2
+    ssl_context.options |= ssl.OP_NO_SSLv3
+
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_REQUIRED
+
+    cacert_path = path / "ca-cert.pem"
+    if cacert_path.exists():
+        logger.debug(f"Loading CA certificate {cacert_path}")
+        ssl_context.load_verify_locations(cacert_path)
+    else:
+        logger.debug("Loading system CA certificates")
+        if ssl_context.verify_mode == ssl.CERT_NONE:
+            ssl_context.verify_mode = ssl.CERT_OPTIONAL
+        ssl_context.load_default_certs()
+
+    logger.info("Check hostname: %s, server verify mode: %s",
+                ssl_context.check_hostname, ssl_context.verify_mode)
+
+    logger.debug("Loading client certificate and key")
+    ssl_context.load_cert_chain(certfile=path / "loader-cert.pem",
+                                keyfile=path / "loader-priv-key.pem",
+                                password=password_loader(path / "key-password.txt"))
+
+    return ssl_context
