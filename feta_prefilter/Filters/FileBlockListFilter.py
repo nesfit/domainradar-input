732d3a541c6785ca6d5797b05275d3574ea1a8fe
Ondrej Lichtner <ilichtner@fit.vutbr.cz>
2024-07-18 20:59:38 +0200

    FileBlockListFilter: use trie for suffix matching
    Signed-off-by: Ondrej Lichtner <ilichtner@fit.vutbr.cz>

--- feta_prefilter/Filters/FileBlockListFilter.py
+++ feta_prefilter/Filters/FileBlockListFilter.py
@@ -1,3 +1,5 @@
+import pygtrie
+
 from feta_prefilter.Filters.BaseFilter import FilterAction
 from feta_prefilter.Filters.BaseFilter import BaseFilter
 
@@ -6,13 +8,18 @@ class FileBlockListFilter(BaseFilter):
     def __init__(self, filter_name:str, filter_result_action=FilterAction.DROP, filename=""):
         super().__init__(filter_name, filter_result_action)
 
+        self.suffix_trie = pygtrie.PrefixSet(factory=pygtrie.StringTrie, separator='.')
+
         with open(filename) as f:
-            self.block_domains = [l.strip() for l in f.readlines()]
+            for domain in f.readlines():
+                reversed_domain = '.'.join(reversed(domain.strip().split('.')))
+                self.suffix_trie.add(reversed_domain)
 
     def filter(self, domains: list[str]) -> list[FilterAction]:
         res = []
         for domain in domains:
-            if domain in self.block_domains:
+            reversed_domain = '.'.join(reversed(domain.strip().split('.')))
+            if reversed_domain in self.suffix_trie:
                 res.append(self.filter_result_action)
             else:
                 res.append(FilterAction.PASS)
