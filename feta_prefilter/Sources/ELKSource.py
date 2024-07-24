110e40433742ef738942621c14956737c9bb1548
Ondrej Lichtner <ilichtner@fit.vutbr.cz>
2024-07-24 17:34:15 +0200

    ELKSource: filter only for A and AAAA dns queries
    Signed-off-by: Ondrej Lichtner <ilichtner@fit.vutbr.cz>

--- feta_prefilter/Sources/ELKSource.py
+++ feta_prefilter/Sources/ELKSource.py
@@ -6,6 +6,7 @@ from feta_prefilter.Sources.BaseSource import BaseSource
 
 logger = logging.getLogger(__name__)
 
+
 class ELKSource(BaseSource):
     def __init__(self, elk_url: str):
         self.es = Elasticsearch(elk_url)
@@ -18,25 +19,33 @@ class ELKSource(BaseSource):
             index="logstash-*",
             query={
                 "bool": {
-                    "must": {
-                        "match": { "event_type": "dns" },
-                        "match": { "dns.type": "query" },
-                    },
+                    "must": [
+                        {"match": {"event_type": "dns"}},
+                        {"match": {"dns.type": "query"}},
+                        {
+                            "bool": {
+                                "should": [
+                                    {"match": {"dns.rrtype": "A"}},
+                                    {"match": {"dns.rrtype": "AAAA"}},
+                                ]
+                            }
+                        },
+                    ],
                     "filter": [
-                        {'range': {'timestamp': {"gt": last_1_day.isoformat()}}},
+                        {"range": {"timestamp": {"gt": last_1_day.isoformat()}}},
                     ],
                 }
             },
             sort=[
                 {"timestamp": "asc"},
             ],
-            search_after = self._latest_sort,
+            search_after=self._latest_sort,
             size=10000,
         )
         logger.debug("FINISH", datetime.utcnow())
         timestamp = None
-        for hit in results.body['hits']['hits']:
-            self._latest_sort = hit['sort']
-            timestamp = hit['_source']['@timestamp']
-            yield hit['_source']['dns']['rrname']
+        for hit in results.body["hits"]["hits"]:
+            self._latest_sort = hit["sort"]
+            timestamp = hit["_source"]["@timestamp"]
+            yield hit["_source"]["dns"]["rrname"]
         logger.debug("Last record from elk from this call", timestamp)
